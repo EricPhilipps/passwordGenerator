cf36b226463c610b19948e914ef80b8a112807c1
Eric Philipps <ericphilipps@Erics-MacBook-Pro.local>
2022-03-24 15:52:05 -0500

    Pseudo Code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+}
+
+function randomizer() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Create the variables to save user input
+  // booleans for whether to include each type of character
+    // Special Characters
+    // Numeric Characters
+    // Lowercase Characters
+    // Uppercase Characters
+    // alert, confirm, and prompt
+    // use conditionals to validate inputs
+      // password length, or no characters selected
+
+  // password length string (but must be integer)
+
+// Make an array for each type of character
+  // Special, numeric, lowercase, uppercase characters
+
+// generatePassword function -- take user input and grab characters
+// from array and generate password
+
+// Use random function to take random characters from each array chosen by user
+
+// create an empty array to become the final password and randomize
+  // then use a for loop to convert the arrray into a string
+
